3229f77e9a21f674a5c5e2e66b9c8d1b9c8e0c26
anilkumar70820 <ak9565191@gmail.com>
2023-11-08 12:43:11 +0530

    ticker slider update

--- src/components/TickerSlider.jsx
+++ src/components/TickerSlider.jsx
@@ -1,41 +1,24 @@
-import React from "react";
+import React from 'react'
+
 const TickerSlider = () => {
+    return (
+        <div class="ticker-wrap my-5">
+            <div class="ticker">
+                <span class="item-collection-1">
+                    <span class="item">Anil Kumar</span>
+                    <span class="item">Anil Kumar</span>
+                    <span class="item">Anil Kumar</span>
+                    <span class="item">Anil Kumar</span>
+                </span>
+                <span class="item-collection-2">
+                    <span class="item">Anil Kumar</span>
+                    <span class="item">Anil Kumar</span>
+                    <span class="item">Anil Kumar</span>
+                    <span class="item">Anil Kumar</span>
+                </span>
+            </div>
+        </div>
+    )
+}
 
-  return (
-    <section class="marquees-wrapper my-5">
-    <div class="marquee marquee-1">
-      <ul class="marquee__content scroll mb-0 py-2">
-        <li>Anil Kumar</li>
-        <li>&#183;</li>
-        <li>Anil Kumar</li>
-        <li>&#183;</li>
-        <li>Anil Kumar</li>
-        <li>&#183;</li>
-        <li>Anil Kumar</li>
-        <li>&#183;</li>
-        <li>Anil Kumar</li>
-        <li>&#183;</li>
-        <li>Anil Kumar</li>
-        <li>&#183;</li>
-      </ul>
-     
-      {/* <ul class="marquee__content scroll mb-0 py-2" aria-hidden="true">
-        <li>Pardeep Dhariwal</li>
-        <li>&#183;</li>
-        <li>Pardeep Dhariwal</li>
-        <li>&#183;</li>
-        <li>Pardeep Dhariwal</li>
-        <li>&#183;</li>
-        <li>Pardeep Dhariwal</li>
-        <li>&#183;</li>
-        <li>Pardeep Dhariwal</li>
-        <li>&#183;</li>
-        <li>Pardeep Dhariwal</li>
-        <li>&#183;</li>
-      </ul> */}
-    </div>
-    </section>
-  );
-};
-
-export default TickerSlider;
+export default TickerSlider
