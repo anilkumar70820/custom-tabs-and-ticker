764159da0e233e748fdb7462a2c31cbdcee73431
anilkumar70820 <ak9565191@gmail.com>
2023-11-09 10:52:53 +0530

    ticker

--- src/components/TickerSlider.jsx
+++ src/components/TickerSlider.jsx
@@ -5,16 +5,16 @@ const TickerSlider = () => {
         <div class="ticker-wrap my-5">
             <div class="ticker">
                 <span class="item-collection-1">
-                    <span class="item">Anil Kumar</span>
-                    <span class="item">Anil Kumar</span>
-                    <span class="item">Anil Kumar</span>
-                    <span class="item">Anil Kumar</span>
+                    <span class="item">Anil Kumar Anil Kumar</span>
+                    <span class="item">Anil Kumar Anil Kumar</span>
+                    <span class="item">Anil Kumar Anil Kumar</span>
+                    <span class="item">Anil Kumar Anil Kumar</span>
                 </span>
                 <span class="item-collection-2">
-                    <span class="item">Anil Kumar</span>
-                    <span class="item">Anil Kumar</span>
-                    <span class="item">Anil Kumar</span>
-                    <span class="item">Anil Kumar</span>
+                    <span class="item">Anil Kumar Anil Kumar</span>
+                    <span class="item">Anil Kumar Anil Kumar</span>
+                    <span class="item">Anil Kumar Anil Kumar</span>
+                    <span class="item">Anil Kumar Anil Kumar</span>
                 </span>
             </div>
         </div>
